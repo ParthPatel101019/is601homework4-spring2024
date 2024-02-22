120cbf2027f0a9de8b77faddee9b988e2f472bdc
ParthPatel101019 <157995349+ParthPatel101019@users.noreply.github.com>
2024-02-21 23:25:24 -0500

    Update calculation.py

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,22 +1,44 @@
-from decimal import Decimal
-from typing import Callable
-from calculator.operations import add, subtract, multiply, divide
+from calculations import Operation
 
 
-class Calculation:
+class Calculator:
 
-    def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
-        self.a = a
-        self.b = b
-        self.operation = operation
+    def __init__(self):
+        self.calculations = Operation()
+
+    def add(self, a, b):
+        self.calculations.add_history(a, b, self.add)
+        return a + b
+
+    def subtract(self, a, b):
+        self.calculations.add_history(a, b, self.subtract)
+        return a - b
+
+    def multiply(self, a, b):
+        self.calculations.add_history(a, b, self.multiply)
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Denominator cannot be zero")
+        # Adding only if there is no error
+        self.calculations.add_history(a, b, self.divide)
+        return round(a / b, 4)
+    
+    def get_history(self):
+        return self.calculations.get_history()
+
+
+
+# def main():
+#     c = Calculator()
+#     print(c.add(1, 3))
+#     print(c.subtract(1, 4))
+#     print(c.subtract(5, 3)    )
+#     print(c.multiply(20, 3))
+#     print(c.divide(10, 2))
+#     print(c.divide(3, 7))
+#     print(c.get_history())
     
 
-    @staticmethod    
-    def create(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
-        return Calculation(a, b, operation)
-
-    def perform(self) -> Decimal:
-        return self.operation(self.a, self.b)
-
-    def __repr__(self):
-        return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
+# main()
